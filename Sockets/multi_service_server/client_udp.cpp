345659e000ccb031d0b69a8147f761b639e3ac28
akashkothi <akashkothi2001@gmail.com>
2022-02-03 22:48:40 +0530

    multi_service_server

---
+++ Sockets/multi_service_server/client_udp.cpp
@@ -0,0 +1,30 @@
+#include "../cn.h"
+
+int main() {
+
+    int sfd;
+    struct sockaddr_in server_addr;
+    socklen_t len = sizeof(server_addr);
+
+    init_socket_address(&server_addr,LOCAL_HOST,ports[0]);
+
+    if((sfd = socket(AF_INET,SOCK_DGRAM,0)) < 0)
+        error("socket error");
+    
+    char buff[BUFFSIZE] = {'\0'};
+
+    cout<<"Enter message : ";
+
+    cin.getline(buff,BUFFSIZE);
+
+    if(sendto(sfd,buff,BUFFSIZE,0,(struct sockaddr*)&server_addr,len) < 0)
+        error("sendto error");
+    cout<<"message sent ..."<<endl;
+
+    if(recvfrom(sfd,buff,BUFFSIZE,0,(struct sockaddr*)&server_addr,&len) < 0)
+        error("recvfrom error");
+    cout<<"message received ..."<<endl;
+
+    cout<<buff<<endl;
+
+}
--- Sockets/multi_service_server/server.cpp
+++ Sockets/multi_service_server/server.cpp
@@ -1,57 +1,127 @@
 #include "../cn.h"
 
+struct pollfd sfd[3];
+
+void fork_child(const char* path, int nsfd) {
+
+    if(fork() == 0) {
+
+        for(int i = 0; i < 3; i++)
+            close(sfd[i].fd);
+        
+        dup2(nsfd,STDIN_FILENO);
+        dup2(nsfd,STDOUT_FILENO);
+        
+        execl(path,path,NULL);    
+    }
+}
+
+void echo(int sfd) {
+
+    struct sockaddr_in client_addr;
+    socklen_t len = sizeof(client_addr);
+    char buff[BUFFSIZE] = {'\0'};
+
+    if(recvfrom(sfd,buff,BUFFSIZE,0,(struct sockaddr*)&client_addr,&len) < 0)
+        error("recvfrom error");
+    cout<<"message received from "<<inet_ntoa(client_addr.sin_addr)<<" "<<client_addr.sin_port<<endl;
+
+    if(sendto(sfd,buff,BUFFSIZE,0,(struct sockaddr*)&client_addr,len) < 0)
+        error("sendto error");
+    cout<<"message sent ..."<<endl;
+}
+
+void* de_capitalize_thread(void *fd) {
+    
+    char buff[BUFFSIZE] = {'\0'};
+    int nsfd = *(int *)fd;
+    
+    if(recv(nsfd,buff,BUFFSIZE,0) < 0)
+        perror("recv error");
+    cout<<"message received ...\n";
+
+    de_capitalize(buff);
+    
+    if(send(nsfd,buff,BUFFSIZE,0) < 0)
+        perror("send error");
+    cout<<"message sent ...\n";
+
+    pthread_exit(NULL);
+}
+
+
 int main() {
 
-    int sfd, nsfd;
-    struct sockaddr_in server_addr,client_addr;
+    int nsfd, th = 0;
+    struct sockaddr_in server_addr[3],client_addr;
     socklen_t len = sizeof(client_addr);
+    pthread_t thread[10];
 
-    init_socket_address(&server_addr,LOCAL_HOST,PORT);
+    init_socket_address(&server_addr[0],LOCAL_HOST,ports[0]);
 
-    if((sfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
+    if((sfd[0].fd = socket(AF_INET,SOCK_DGRAM,0)) < 0)
         error("socket error");
-    
-    if(bind(sfd,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0)
+        
+    if(bind(sfd[0].fd,(struct sockaddr *)&server_addr[0],len) < 0)
         error("bind error");
 
-    if(listen(sfd,BACKLOG) < 0) 
-        error("listen error");
-    
-    for(int i = 0; i < 3; i++) {
+
+    for(int i = 1; i < 3; i++) {
         
-        if((nsfd = accept(sfd,(struct sockaddr *)&client_addr,&len)) < 0)
-            error("accept error");
+        init_socket_address(&server_addr[i],LOCAL_HOST,ports[i]);
+        
+        if((sfd[i].fd = socket(AF_INET,SOCK_STREAM,0)) < 0)
+            error("socket error");
+        
+        if(bind(sfd[i].fd,(struct sockaddr *)&server_addr[i],len) < 0)
+            error("bind error");
 
-        cout<<"\nconnection established ..."<<endl;
-
-        int service = 0;
-
-        if(recv(nsfd,&service,sizeof(service),0) < 0)
-            error("recv error");
-
-        if(fork() == 0) {
-            
-            close(sfd);
-            dup2(nsfd,STDIN_FILENO);
-            dup2(nsfd,STDOUT_FILENO);
-            close(nsfd);
-            
-            switch(service) {
-                case 1:
-                    execl("./service_1.exe","./service_1.exe",NULL);
-                    break;
-                case 2:
-                    execl("./service_2.exe","./service_2.exe",NULL);
-                    break;
-                default:
-                    execl("./service_1.exe","./service_1.exe",NULL);
-                    break;
-            }
-
-        }
+        if(listen(sfd[i].fd,BACKLOG) < 0) 
+            error("listen error");
     }
-
-    for(int i = 0; i < 3; i++)
-        wait(NULL); 
     
+
+    for(int i = 0; i < 3; i++) 
+        sfd[i].events = POLLRDNORM;
+
+
+    while(1) {
+
+        if(poll(sfd,3,500) < 0)
+            error("poll error");
+        else {
+
+            for(int i = 0; i < 3; i++) {
+                if(sfd[i].revents == POLLRDNORM) {    
+                    
+                    switch(i) {
+
+                        case 0: {
+                            echo(sfd[0].fd);
+                            break;
+                        }
+
+                        case 1: {
+                            if((nsfd = accept(sfd[i].fd,(struct sockaddr*)&client_addr,&len)) < 0)
+                                error("accept error");
+                            cout<<"connection established ..."<<endl;
+                            fork_child("./capitalize.exe",nsfd);                            
+                            break;
+                        }
+
+                        case 2: {
+                            if((nsfd = accept(sfd[i].fd,(struct sockaddr*)&client_addr,&len)) < 0)
+                                error("accept error");
+                            cout<<"connection established ..."<<endl;
+                            pthread_create(&thread[th++],NULL,de_capitalize_thread,&nsfd);    
+                            break;
+                        }
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }   
 }
